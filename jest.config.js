e0a6db80bab983615fc0d89bf8dd40fb513ed56a
Benjamin Swanzey <swanzeyb2001@gmail.com>
2025-04-26 17:57:30 -0700

    vi test

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: "node",
-  testMatch: ["**/nodes/**/*.test.ts"],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest"
-  },
-  moduleFileExtensions: ["ts", "js", "json"]
-};
